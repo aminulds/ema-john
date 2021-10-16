1aedef9f7085733dc3e0e634fc9eb50032f98237
Aminul Islam <aminulds61@gmail.com>
2021-10-16 10:57:19 +0600

    added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import logo from '../../images/logo.png';
-import './header.css';
+import React from "react";
+import logo from "../../images/logo.png";
+import "./header.css";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <img src={logo} alt="" />
-            <nav>
-                <a href="/shop">Shop</a>
-                <a href="/review">Order Review</a>
-                <a href="/manage">Manage Inventory</a>
-            </nav>
-        </div>
-    );
+  return (
+    <div className="header">
+      <img src={logo} alt="" />
+      <nav>
+        <a href="/shop">Shop</a>
+        <a href="/review">Order Review</a>
+        <a href="/manage">Manage Inventory</a>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,33 +1,33 @@
-import React, { useState } from 'react';
-import fakeData from '../../fakeData';
-import Cart from '../Cart/Cart';
-import Product from '../Product/Product';
-import './shop.css';
+import React, { useState } from "react";
+import fakeData from "../../fakeData";
+import Cart from "../Cart/Cart";
+import Product from "../Product/Product";
+import "./shop.css";
 
 const Shop = () => {
-    const first20 = fakeData.slice(0, 5);
-    const [products, setProducts] = useState(first20);
-    //Handle Add Product
-    const [cart, setCart] = useState([]);
+  const first20 = fakeData.slice(0, 5);
+  const [products, setProducts] = useState(first20);
+  //Handle Add Product
+  const [cart, setCart] = useState([]);
 
-    const handleAddProduct = (product) => setCart([...cart, product]);
+  const handleAddProduct = (product) => setCart([...cart, product]);
 
-    return (
-        <div className="shop-container">
-            <div className="product-container">
-                {
-                    products.map(product => <Product 
-                        handleAddProduct = {handleAddProduct}
-                        productDetails={product}
-                        ></Product> )
-                }
-            </div>
+  return (
+    <div className="shop-container">
+      <div className="product-container">
+        {products.map((product) => (
+          <Product
+            handleAddProduct={handleAddProduct}
+            productDetails={product}
+          ></Product>
+        ))}
+      </div>
 
-            <div className="cart-container">
-                <Cart cart={cart}></Cart>
-            </div>
-        </div>
-    );
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
